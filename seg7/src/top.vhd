acd22ebb38f835cf2d6ee6a9cb6afb6dc656d03b
Bert Berrevoets <bert@berrevoets.net>
2022-12-26 12:45:31 +0100

    Constants and vector indexing
    We declare constant values in the declarative region of a process, and we use them to index a vector of bits.

--- seg7/src/top.vhd
+++ seg7/src/top.vhd
@@ -17,17 +17,36 @@ ARCHITECTURE rtl OF top IS
 
 BEGIN
 
-  digit <= 7;
+  digit <= 0;
 
   ENCODER_PROC : PROCESS (digit)
+
+    -- Segments from the PmodSSD datasheet
+    CONSTANT A : INTEGER := 0;
+    CONSTANT B : INTEGER := 1;
+    CONSTANT C : INTEGER := 2;
+    CONSTANT D : INTEGER := 3;
+    CONSTANT E : INTEGER := 4;
+    CONSTANT F : INTEGER := 5;
+    CONSTANT G : INTEGER := 6;
+
   BEGIN
     CASE digit IS
+
+      WHEN 0 =>
+        segments(A) <= '1';
+        segments(B) <= '1';
+        segments(C) <= '1';
+        segments(D) <= '1';
+        segments(E) <= '1';
+        segments(F) <= '1';
+        segments(G) <= '0';
 
       WHEN 8 =>
         segments <= "1111111";
 
       WHEN OTHERS =>
-      segments <= "0000000";
+        segments <= "0000000";
 
     END CASE;
   END PROCESS;
