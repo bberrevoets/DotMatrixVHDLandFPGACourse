2780d36fc3dc9cc0efe849d53ac23d9dd878542e
Bert Berrevoets <bert@berrevoets.net>
2022-12-26 15:06:04 +0100

    Create a testbench and simulate in ModelSim
    Before we can simulate our VHDL module, we need to create a VHDL test program called a testbench.

--- seg7/src/top.vhd
+++ seg7/src/top.vhd
@@ -1,83 +1,83 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-ENTITY top IS
-  PORT (
-    clk : IN STD_LOGIC;
-    rst_n : IN STD_LOGIC;
-    segments : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
-    digit_sel : OUT STD_LOGIC
+entity top is
+  port (
+    clk : in std_logic;
+    rst_n : in std_logic;
+    segments : out std_logic_vector(6 downto 0);
+    digit_sel : out std_logic
   );
-END top;
+end top;
 
-ARCHITECTURE rtl OF top IS
+architecture rtl of top is
 
-  SIGNAL digit : INTEGER;
+  signal digit : integer;
 
-BEGIN
+begin
 
   digit <= 7;
 
-  ENCODER_PROC : PROCESS (digit)
+  ENCODER_PROC : process (digit)
 
     -- Segments from the PmodSSD datasheet
-    CONSTANT A : INTEGER := 0;
-    CONSTANT B : INTEGER := 1;
-    CONSTANT C : INTEGER := 2;
-    CONSTANT D : INTEGER := 3;
-    CONSTANT E : INTEGER := 4;
-    CONSTANT F : INTEGER := 5;
-    CONSTANT G : INTEGER := 6;
+    constant A : integer := 0;
+    constant B : integer := 1;
+    constant C : integer := 2;
+    constant D : integer := 3;
+    constant E : integer := 4;
+    constant F : integer := 5;
+    constant G : integer := 6;
 
-  BEGIN
-    segments <= (OTHERS => '1');
+  begin
+    segments <= (others => '1');
 
-    CASE digit IS
+    case digit is
 
-      WHEN 0 =>
+      when 0 =>
         segments(G) <= '0';
 
-      WHEN 1 =>
-        segments <= (OTHERS => '0');
+      when 1 =>
+        segments <= (others => '0');
         segments(B) <= '1';
         segments(C) <= '1';
 
-      WHEN 2 =>
+      when 2 =>
         segments(C) <= '0';
         segments(F) <= '0';
 
-      WHEN 3 =>
+      when 3 =>
         segments(E) <= '0';
         segments(F) <= '0';
 
-      WHEN 4 =>
+      when 4 =>
         segments(A) <= '0';
         segments(D) <= '0';
         segments(E) <= '0';
 
-      WHEN 5 =>
+      when 5 =>
         segments(B) <= '0';
         segments(E) <= '0';
 
-      WHEN 6 =>
+      when 6 =>
         segments(B) <= '0';
 
-      WHEN 7 =>
+      when 7 =>
         segments(D) <= '0';
         segments(E) <= '0';
         segments(F) <= '0';
         segments(G) <= '0';
 
-      WHEN 8 =>
+      when 8 =>
 
-      WHEN 9 =>
+      when 9 =>
         segments(E) <= '0';
 
-      WHEN OTHERS =>
-        segments <= (OTHERS => '0');
+      when others =>
+        segments <= (others => '0');
 
-    END CASE;
-  END PROCESS;
+    end case;
+  end process;
 
-END ARCHITECTURE;
+end architecture;
